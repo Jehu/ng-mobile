db114a7ffb403cced50f2c316762cccc04d60c16
Jehu <marco.michely@googlemail.com>
2013-06-08 16:08:44 +0200

    make a new start

---
+++ config.rb
@@ -0,0 +1,13 @@
+http_path = "/"
+css_dir = "/"
+sass_dir = "src"
+images_dir = "img"
+javascripts_dir = "js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+preferred_syntax = :sass
+# relative_assets = true
+# line_comments = false
+
---
+++ ng-mobile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('ngMobile', ['ajoslin.mobile-navigate'])
+.run(['$rootScope', '$route', '$http', '$templateCache', '$timeout', '$window', '$location', function($rootScope, $route, $http, $templateCache, $timeout, $window, $location) {
+	$rootScope.scrollPos = [];
+
+	// Cache all templates on App Start
+	angular.forEach($route.routes, function(r) {
+		if (r.templateUrl) {
+			$http.get(r.templateUrl, {cache: $templateCache});
+		}
+	});
+
+	// Generic solution to save/restore scroll position START
+	$rootScope.scrollPos = {}; // scroll position of each view
+
+	$window.addEventListener('scroll', function() {
+		if ($rootScope.okSaveScroll) { // false between $routeChangeStart and $routeChangeSuccess
+			$rootScope.scrollPos[escape($location.path())] = $window.scrollY;
+		}
+	});
+
+	$rootScope.$on('$routeChangeStart', function(e, cur, prev) {
+		if(prev !== undefined && prev.hasOwnProperty('scope') && prev.scope !== undefined) {
+			prev.scope.$destroy();
+		}
+		$rootScope.okSaveScroll = false;
+	});
+
+	$rootScope.$on('$routeChangeSuccess', function() {
+		$timeout(function() {
+			window.FastClick.attach(document.body);
+			return;
+		},0);
+
+		$timeout(function() { // wait for DOM, then restore scroll position
+			$window.scrollTo(0, ($rootScope.scrollPos[escape($location.path())] !== undefined) ? $rootScope.scrollPos[escape($location.path())] : 0);
+			$rootScope.okSaveScroll = true;
+			return;
+		},0);
+	});
+	// Generic solution for scroll position END
+}]);
+
